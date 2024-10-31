dc63eaf733bf8a583384ab87c6f27578b25fafe9
Erick Henrique <erickhen567@gmail.com>
2024-10-30 21:28:36 -0300

    Add files via upload

---
+++ aula11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float x[10] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
+    float *ptx;
+
+    for (ptx = &x[0]; ptx <= &x[9]; ptx++)
+    {
+        printf("\n %f", *ptx);
+    }
+
+
+    return 0;
+}
