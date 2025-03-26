8fed8033835fefdc79027c77fdedc79e28a3da42
Erick Henrique <erickhen567@gmail.com>
2025-03-25 22:37:45 -0300

    Update Segunda Tabela.sql

--- Segunda Tabela.sql
+++ Segunda Tabela.sql
@@ -1,19 +1,19 @@
 create database BibliotecaDB;
 use BibliotecaDB;
 
-create table Autor(
+create table Autores(
 	id_autor int auto_increment primary key (id_autor),
     nome varchar(50) not null,
     sobrenome varchar(50) not null,
     nacionalidade varchar(30)
 );
 
-create table Categoria(
+create table Categorias(
 	id_categoria int auto_increment primary key (id_categoria),
     descricao varchar(50) not null
 );
 
-create table Livro(
+create table Livros(
 	id_livro int auto_increment primary key (id_livro),
     titulo varchar(100) not null,
     ano_publicacao year
